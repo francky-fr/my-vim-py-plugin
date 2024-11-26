4ac98fabec28aae2436a630fd8f6501ac421634e
franck <franck.levy@skillcorner.com>
2024-11-26 23:04:05 +0100

    aaa

---
+++ nvim-setup/lua/color-setup.lua
@@ -0,0 +1,4 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "python", "lua", "vim" }, -- Add your languages here
+  highlight = { enable = true },
+}
---
+++ nvim-setup/lua/venv-setup.lua
@@ -0,0 +1,8 @@
+require("venv-selector").setup({
+  auto_sources = true, -- Automatically detect virtual environments
+  search = true,       -- Search for virtual environments in default locations
+  -- search_paths = {
+  --   "~/git_rep/my-vim-py-plugin"
+  -- },
+})
+
