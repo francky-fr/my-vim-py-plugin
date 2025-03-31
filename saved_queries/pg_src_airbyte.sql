ec41d9ab316720fad8213de5773fa2076f6ab46e
franck <franck.levy@skillcorner.com>
2025-03-31 17:48:30 +0200

    Update saved_query pg

--- saved_queries/pg_src_airbyte.sql
+++ saved_queries/pg_src_airbyte.sql
@@ -1,9 +1,18 @@
+-- Setup phase
 CREATE USER airbyte PASSWORD <>;
 
 GRANT USAGE ON SCHEMA public TO airbyte;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO airbyte;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO airbyte;
 
--- ALTER USER airbyte REPLICATION
+-- ALTER USER airbyte REPLICATION => What docs provides, but N/A on RDS, use instead :
 GRANT rds_replication TO airbyte;
+
+SELECT pg_create_logical_replication_slot('airbyte_slot', 'pgoutput');
+ALTER TABLE api_player REPLICA IDENTITY DEFAULT;
+CREATE PUBLICATION airbyte_publication FOR TABLE api_player;
+
+
+-- Monitoring
+SELECT * FROM pg_replication_slots WHERE slot_name = 'airbyte_slot';
 
